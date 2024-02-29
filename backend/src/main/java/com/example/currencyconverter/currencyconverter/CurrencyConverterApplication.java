d466eb4b37156ca45108dd9d940cb0725899bbb6
Kalyan Soma <diddisusreeja15@gmail.com>
2024-02-29 17:55:39 -0600

    susreeja

---
+++ backend/src/main/java/com/example/currencyconverter/currencyconverter/CurrencyConverterApplication.java
@@ -0,0 +1,13 @@
+package com.example.currencyconverter.currencyconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConverterApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/currencyconverter/currencyconverter/CurrencyConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.currencyconverter.currencyconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
