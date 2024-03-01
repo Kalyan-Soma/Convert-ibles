44b6f457a5008a61504b8753208b5e9c2a357602
Kalyan Soma <diddisusreeja15@gmail.com>
2024-02-29 20:08:17 -0600

    Susreeja

--- backend/src/main/java/com/currencyconverter/currencyconverter/Application.java
+++ backend/src/main/java/com/currencyconverter/currencyconverter/Application.java
@@ -1,13 +1,13 @@
-package com.example.currencyconverter.currencyconverter;
+package com.currencyconverter.currencyconverter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CurrencyConverterApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CurrencyConverterApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
