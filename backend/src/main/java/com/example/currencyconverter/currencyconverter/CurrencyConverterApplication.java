0c95ebc9c747030c8185ed33e155f2567de9e6f8
Kalyan Soma <kalyan.soma2022@gmail.com>
2024-02-29 19:30:30 -0600

    Deleted unwanted files

--- backend/src/main/java/com/example/currencyconverter/currencyconverter/CurrencyConverterApplication.java
+++ backend/src/main/java/com/example/currencyconverter/currencyconverter/CurrencyConverterApplication.java
@@ -6,8 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CurrencyConverterApplication {
 
-	public static void main(String[] args) {
+	private static final String Hello = null;
+
+    public static void main(String[] args) {
 		SpringApplication.run(CurrencyConverterApplication.class, args);
+        System.out.println("Hello");
+        return;
 	}
 
 }
