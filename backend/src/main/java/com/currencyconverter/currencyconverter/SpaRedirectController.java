6a09c1b310e57adedc78a8aec3fa1d0984753406
Aneesh Prodduturu <aneeshpavan@gmail.com>
2024-03-11 19:27:45 -0500

    Aneesh

--- backend/src/main/java/com/currencyconverter/currencyconverter/SpaRedirectController.java
+++ backend/src/main/java/com/currencyconverter/currencyconverter/SpaRedirectController.java
@@ -6,14 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class SpaRedirectController {
 
-    /**
-     * Catch all route that does not contain a dot (.) to distinguish it from static
-     * resources.
-     * It forwards every request to the root URL ("/"), letting the SPA handle the
-     * routing.
-     * 
-     * @return A forward directive to the root URL ("/").
-     */
     @RequestMapping(value = "/**/{path:[^\\.]*}")
     public String redirectToIndex() {
         return "forward:/";
