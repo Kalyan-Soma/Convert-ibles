a919c77e60634aaf423fbdbe2c4a3b944058c5f0
Aneesh Prodduturu <aneeshpavan@gmail.com>
2024-03-11 19:08:23 -0500

    Fix

--- backend/src/main/java/com/currencyconverter/currencyconverter/CurrencyConversionController.java
+++ backend/src/main/java/com/currencyconverter/currencyconverter/CurrencyConversionController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 
 @RestController
 public class CurrencyConversionController {
